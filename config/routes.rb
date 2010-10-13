6451945694a6a57784c82aaeecbdfbf5c3510a64
roca <RomelCampbell@gmail.com>
2010-10-13 16:50:54 -0400

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
   get "pages/home"
 
   get "pages/contact"
@@ -62,5 +64,11 @@ SampleApp::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
   
-  match '/', :to => 'pages#home'
+  match '/signup',  :to => 'users#new'
+  
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+
+  root :to => 'pages#home'
 end

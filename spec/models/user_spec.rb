4de51dcc093ad58e6c3cdbfed1a31ddf1eb29d50
roca <RomelCampbell@gmail.com>
2010-10-15 14:29:06 -0400

    Started Validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should create a new instance given a valid attribute" do
+      User.create!(:name => "Excample user", :email => "user.example.com")
+  end
+  
+  it "should require a name" do
+    no_name_user = User.new(:email => "user@example.com")
+    no_name_user.should_not be_valid
+  end
+  
+  
+  it "should require a name" do
+    no_email_user = User.new(:name => "Excample user")
+    no_email_user.should_not be_valid
+  end
+  
 end

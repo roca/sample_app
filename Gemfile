5cfe5fb0328e56c943fd065240749ac4eede61ec
roca <RomelCampbell@gmail.com>
2010-10-22 14:07:35 -0400

    Added a User factory and tests for the show page

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ end
 group :test do 
 	gem 'rspec', '2.0.0.beta.18'
 	gem 'spork', '0.8.4'
+	gem 'factory_girl_rails','1.0'
 end
 
 # Bundle edge Rails instead:
---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                   "Romel Campbell"
+  user.email                  "RomelCampbell@gmail.com"
+  user.password               "foobar"
+  user.password_confirmation  "foobar"
+end

172caf2c8d3d1a0505ac0cd244e79b35cab281fa
roca <RomelCampbell@gmail.com>
2010-10-27 17:31:49 -0400

    Finished user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,18 @@ class UsersController < ApplicationController
   end
   
   def show
-      @user = User.find(params[:id])
+      @user  = User.find(params[:id])
       @title = @user.name
   end
   
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to @user, :flash => { :success => "Welcome to the Sample App!" }
+    else
+     @title = "Sign up"
+     render 'new'
+   end
+  end
 
 end

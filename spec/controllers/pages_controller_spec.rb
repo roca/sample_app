cce6cf0ee6c45f9bc41f0608a258c450470342d5
roca <RomelCampbell@gmail.com>
2011-03-06 19:16:43 -0500

    Chapter 3 Excercizes Done

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -36,7 +36,7 @@ describe PagesController do
       it "should have the right title" do 
         get 'home' 
         response.should have_selector("title", 
-              :content => "#{@base_title} | Home")
+              :content => @base_title + " | Home")
       end
       
       it "should have a non-blank body" do
@@ -81,7 +81,7 @@ describe PagesController do
     it "should have the right title" do 
       get 'contact' 
       response.should have_selector("title", 
-            :content => "Ruby on Rails Tutorial Sample App | Contact")
+            :content => @base_title + " | Contact")
     end
   end
   
@@ -94,9 +94,22 @@ describe PagesController do
     it "should have the right title" do 
       get 'about' 
       response.should have_selector("title", 
-            :content => "Ruby on Rails Tutorial Sample App | About")
+            :content => @base_title + " | About")
     end
   end
   
+
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+    
+    it "should have the right title" do 
+      get 'help' 
+      response.should have_selector("title", 
+            :content => @base_title + " | Help")
+    end
+  end
 
 end

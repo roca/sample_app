0b85187afb3632447e1f42344cdd2ec3470987e0
roca <RomelCampbell@gmail.com>
2011-12-23 09:29:09 -0500

    Removed email setings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,16 +58,16 @@ SampleApp31::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   config.assets.debug = true
-  
-  
-  
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :user_name            => 'RomelCampbell@gmail.com',
-    :password             => 'jGQx8TG2XCWrN',
-    :authentication       => 'plain',
-    :enable_starttls_auto => true  }
-  
+    # 
+    # 
+    # 
+    # config.action_mailer.delivery_method = :smtp
+    # config.action_mailer.smtp_settings = {
+    #   :address              => "smtp.gmail.com",
+    #   :port                 => 587,
+    #   :user_name            => 'RomelCampbell@gmail.com',
+    #   :password             => 'jGQx8TG2XCWrN',
+    #   :authentication       => 'plain',
+    #   :enable_starttls_auto => true  }
+    
 end

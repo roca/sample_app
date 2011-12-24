3d99c2f4d674d16bcbccefefd2a82ff1f8b30fa5
roca <RomelCampbell@gmail.com>
2011-12-24 05:16:57 -0500

    Added back email functionality

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,13 +27,13 @@ SampleApp31::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
-  # config.action_mailer.delivery_method = :smtp
-  #  config.action_mailer.smtp_settings = {
-  #    :address              => "smtp.gmail.com",
-  #    :port                 => 587,
-  #    :user_name            => 'RomelCampbell@gmail.com',
-  #    :password             => 'jGQx8TG2XCWrN',
-  #    :authentication       => 'plain',
-  #    :enable_starttls_auto => true  }
-   
+  config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :user_name            => 'RomelCampbell@gmail.com',
+      :password             => 'jGQx8TG2XCWrN',
+      :authentication       => 'plain',
+      :enable_starttls_auto => true  }
+    
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -123,4 +123,14 @@ SampleApp31::Application.configure do
 
     # Send deprecation notices to registered listeners
     config.active_support.deprecation = :notify
+    
+      config.action_mailer.delivery_method = :smtp
+         config.action_mailer.smtp_settings = {
+           :address              => "smtp.gmail.com",
+           :port                 => 587,
+           :user_name            => 'RomelCampbell@gmail.com',
+           :password             => 'jGQx8TG2XCWrN',
+           :authentication       => 'plain',
+           :enable_starttls_auto => true  }
+      
 end
